3fe6584a83465fdb392fb24af03196e74fb73f42
Myles Borins <myles@famo.us>
2014-09-09 18:30:18 -0700

    It all works, with tests

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-module.exports = 'I DO NOT DO ANYTHING!!!';
+'use strict';
+
+var transformTools = require('browserify-transform-tools');
+
+var options = {
+  jsFilesOnly: true
+};
+
+module.exports  =  transformTools.makeRequireTransform('provify', options,
+  function (args, opts, done) {
+    if (args[0] === 'tap' || args[0] === 'tape') {
+      return done(null, 'require(\'prova\')');
+    }
+    else {
+      return done();
+    }
+  });
--- test/test.js
+++ test/test.js
@@ -1,7 +1,34 @@
+var path = require('path');
 var test = require('tap').test;
+
+var transformTools = require('browserify-transform-tools');
 var provify = require('../');
 
-test('The Module Should not do anything', function (t) {
+var dummy = path.resolve(__dirname, '../fixtures/dummy.js');
+var contentTap = 'require(\'tap\');';
+var contentTape = 'require(\'tape\');';
+var contentUnrelated = 'require(\'hacktheplanet\');';
+
+test('tap requires should be transformed', function (t) {
   t.plan(1);
-  t.equal(provify, 'I DO NOT DO ANYTHING!!!');
+  transformTools.runTransform(provify, dummy, {content: contentTap}, function (err, transformed) {
+    if (err) { throw new Error(err); }
+    t.equal(transformed, 'require(\'prova\');');
+  });
+});
+
+test('tape requires should be transformed', function (t) {
+  t.plan(1);
+  transformTools.runTransform(provify, dummy, {content: contentTape}, function (err, transformed) {
+    if (err) { throw new Error(err); }
+    t.equal(transformed, 'require(\'prova\');');
+  });
+});
+
+test('unrelated requires should not be transformed', function (t) {
+  t.plan(1);
+  transformTools.runTransform(provify, dummy, {content: contentUnrelated}, function (err, transformed) {
+    if (err) { throw new Error(err); }
+    t.equal(transformed, contentUnrelated);
+  });
 });
